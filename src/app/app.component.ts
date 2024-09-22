f61de083ddd316f19755cf88d07db25a6468b8c0
architjain808 <architjain808@gmail.com>
2024-09-22 14:13:55 +0530

    [test] api working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  resCode!: any;
   ngOnInit(): void {
-    fetch('https://cafe-management-be-brrg.onrender.com').then((res) => {
-      console.log(res)
-      debugger;
+    fetch('https://cafe-management-be-brrg.onrender.com/')
+    .then(response => {
+      if (response.ok) {
+        return response.text(); // Convert response body to text
+      }
+      throw new Error('Network response was not ok');
+    })
+    .then(data => {
+      this.resCode = data
+      console.log(data); // This is your expected string
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
     });
   }
   title = 'cafe-management-fe';
